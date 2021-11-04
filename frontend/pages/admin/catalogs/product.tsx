66089f2f1bb45462cb710b0137a9ac5f82881dd4
Anish <atechchatterjeee@gmail.com>
2021-11-04 10:53:13 +0530

    refactored

--- frontend/pages/admin/catalogs/product.tsx
+++ frontend/pages/admin/catalogs/product.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { NextPage } from "next";
-import { Container, Flex } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
 import AddProduct from "../../../components/Admin/AddProduct";
 import Navbar from "../../../components/Admin/Navbar";
-import constants from "../../../util/Constants";
-import axios from "axios";
 
 interface Products {
   name: string;
@@ -18,32 +16,13 @@ interface Category {
   subCategory: string;
 }
 
-// getting all the different available categories including sub-categories
-const getAllCategory = async (): Promise<Category> => {
-  try {
-    const res = await axios.get(`${constants.url}/shop/getAllCategory/`);
-    return Promise.resolve(res.data);
-  } catch (err) {
-    return Promise.reject(err);
-  }
-};
-
-const getAllProducts = async (): Promise<Products> => {
-  try {
-    const res = await axios.get(`${constants.url}/shop/getAllProducts/`);
-    return Promise.resolve(res.data);
-  } catch (err) {
-    return Promise.reject(err);
-  }
-};
-
 const Product: NextPage = () => {
   return (
     <>
       <Navbar />
-      <Flex>
+      <Center>
         <AddProduct marginTop="3em" />
-      </Flex>
+      </Center>
     </>
   );
 };
