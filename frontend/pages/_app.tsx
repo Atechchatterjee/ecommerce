aa19729d24307301124da46c04c7608dc8644f1f
Atechchatterjee <atechchatterjeee@gmail.com>
2022-01-19 15:27:53 +0000

    feat: added the CustomButtonTheme

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,10 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { CookiesProvider } from "react-cookie";
+import CustomButtonTheme from "../components/Custom/CustomButton";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={CustomButtonTheme}>
       <CookiesProvider>
         <Component {...pageProps} />
       </CookiesProvider>
