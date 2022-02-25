edce0794a666d106fdd68bdf8b579fd4da99d5dc
Atechchatterjee <atechchatterjeee@gmail.com>
2022-02-25 22:17:28 +0000

    chore: comment out production url (temporarily)

--- frontend/util/Constants.ts
+++ frontend/util/Constants.ts
@@ -1,7 +1,8 @@
 export default {
   url:
-    process.env.NODE_ENV === 'production'
-      ? process.env.NEXT_PUBLIC_PROD_URL
-      : process.env.NEXT_PUBLIC_URL,
+    // process.env.NODE_ENV === 'production'
+    //   ? process.env.NEXT_PUBLIC_PROD_URL
+      // :
+       process.env.NEXT_PUBLIC_URL,
   googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
 };
