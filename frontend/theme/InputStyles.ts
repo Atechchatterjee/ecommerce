abd08d1adaf369e9bfff5c8112b16780f74a45e9
Atechchatterjee <atechchatterjeee@gmail.com>
2022-03-18 21:02:11 +0000

    style: global styles for input

---
+++ frontend/theme/InputStyles.ts
@@ -0,0 +1,11 @@
+export default {
+  baseStyle: {
+    field: {
+      focusBorderColor: "secondary.200",
+      color: "primary.200",
+    }, 
+    defaultProps: {
+      focusBorderColor: "secondary.200",
+    }
+  }
+}
