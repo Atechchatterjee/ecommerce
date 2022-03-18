df108ac99caa509eda6a29d0c2685db2974f3db7
Atechchatterjee <atechchatterjeee@gmail.com>
2022-03-18 21:02:39 +0000

    style: add input styles

--- frontend/theme/index.ts
+++ frontend/theme/index.ts
@@ -1,10 +1,20 @@
 import { extendTheme } from "@chakra-ui/react";
 import ButtonStyles from "./ButtonStyles";
 import ColorPalatte from "./ColorPalatte";
+import InputStyles from "./InputStyles";
 
 export default extendTheme({
   colors: ColorPalatte,
   components: {
-    Button: ButtonStyles
+    Button: ButtonStyles,
+    Input: InputStyles
   },
+  styles: {
+    global: {
+      Input: {
+        backgroundColor: "red",
+        focusBorderColor: "red"
+      },
+    }
+  }
 });
