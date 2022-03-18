626a98ecef49a807d7f12c7af37d42e1e431da46
Atechchatterjee <atechchatterjeee@gmail.com>
2022-03-18 21:13:42 +0000

    style: change container to custom container

--- frontend/pages/admin/login.tsx
+++ frontend/pages/admin/login.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
-import { Button, Center, Stack, Container } from "@chakra-ui/react";
+import { Button, Center, Stack, Container, Box } from "@chakra-ui/react";
 import { Formik } from "formik";
 import { NextPage } from "next";
 import { CustomField } from "../../components/Custom/CustomField";
 import constants from "../../util/Constants";
 import axios from "axios";
 import Banner from "../../components/Layout/Banner";
+import CustomContainer from "../../components/Custom/CustomContainer";
 
 interface FormData {
   email: string;
@@ -40,13 +41,7 @@ const AdminLogin: NextPage = () => {
   return (
     <div>
       <Banner text="Admin Login" />
-      <Container
-        style={{
-          boxShadow: "0.2em 0.2em 0.2em 0.2em #e1e1e1",
-          height: "23em",
-        }}
-        borderRadius="lg"
-      >
+      <CustomContainer height="23em" borderRadius="sm">
         <Center color="gray" style={{ marginTop: "7vh" }}>
           <Stack
             direction={["row", "column"]}
@@ -66,12 +61,12 @@ const AdminLogin: NextPage = () => {
               {({ handleSubmit, handleChange }) => {
                 return (
                   <form onSubmit={handleSubmit}>
-                    <div style={{ marginTop: "1em" }}>
+                    <Box marginTop="1em">
                       <CustomField
                         placeholder="Admin Email"
                         name="email"
                         label="Admin Email Id"
-                        borderRadius="md"
+                        borderRadius="sm"
                         onChange={handleChange}
                         isInvalid={err}
                       />
@@ -82,28 +77,28 @@ const AdminLogin: NextPage = () => {
                         type="password"
                         label="Admin Password"
                         onChange={handleChange}
-                        borderRadius="md"
+                        borderRadius="sm"
                         isInvalid={err}
                       />
                       <br />
                       <Button
                         marginTop="1em"
                         type="submit"
-                        variant="blueSolid"
+                        variant="primarySolid"
                         width="100%"
-                        borderRadius="lg"
+                        borderRadius="sm"
                       >
                         Submit
                       </Button>
                       <br />
-                    </div>
+                    </Box>
                   </form>
                 );
               }}
             </Formik>
           </Stack>
         </Center>
-      </Container>
+      </CustomContainer>
     </div>
   );
 };
