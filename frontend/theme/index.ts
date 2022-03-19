1581f8383cc83e3916b43eaf56bac5cf03dee0bc
Atechchatterjee <atechchatterjeee@gmail.com>
2022-03-19 21:51:39 +0000

    style: add check box styles

--- frontend/theme/index.ts
+++ frontend/theme/index.ts
@@ -2,12 +2,14 @@ import { extendTheme } from "@chakra-ui/react";
 import ButtonStyles from "./ButtonStyles";
 import ColorPalatte from "./ColorPalatte";
 import InputStyles from "./InputStyles";
+import CheckBoxStyles from "./CheckBoxStyles";
 
 export default extendTheme({
   colors: ColorPalatte,
   components: {
     Button: ButtonStyles,
-    Input: InputStyles
+    Input: InputStyles,
+    Checkbox: CheckBoxStyles,
   },
   styles: {
     global: {
