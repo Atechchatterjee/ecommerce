821f5b02c9e15f19ee83b89fd3d5e2f0c1d2eaf4
Atechchatterjee <atechchatterjeee@gmail.com>
2022-03-25 22:55:32 +0000

    chore: remove unnecessary state variables

--- frontend/components/Custom/CustomSlider.tsx
+++ frontend/components/Custom/CustomSlider.tsx
@@ -6,36 +6,22 @@ import {
   SliderThumb,
   Box,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
 interface CustomSliderProps extends SliderProps {
   icon?: React.ReactNode;
 }
 
-const CustomSlider = ({ icon, children, ...props }: CustomSliderProps) => {
-  const [sliderValue, setSliderValue] = useState<number>(0);
-
-  return (
-    <Slider
-      defaultValue={60}
-      min={0}
-      max={3}
-      step={1}
-      onChange={(value) => {
-        setSliderValue(value);
-      }}
-      {...props}
-    >
-      {children}
-      <SliderTrack bg="gray.300" opacity="0.8">
-        {icon ? icon : <SliderFilledTrack bg="secondary.200" />}
-      </SliderTrack>
-      <SliderThumb boxSize={8}>
-        <Box color="secondary.200" as={FaShoppingCart} size={15} />
-      </SliderThumb>
-    </Slider>
-  );
-};
+const CustomSlider = ({ icon, children, ...props }: CustomSliderProps) => (
+  <Slider defaultValue={60} min={0} max={3} step={1} {...props}>
+    {children}
+    <SliderTrack bg="gray.300" opacity="0.8">
+      {icon ? icon : <SliderFilledTrack bg="secondary.200" />}
+    </SliderTrack>
+    <SliderThumb boxSize={8}>
+      <Box color="secondary.200" as={FaShoppingCart} size={15} />
+    </SliderThumb>
+  </Slider>
+);
 
 export default CustomSlider;
