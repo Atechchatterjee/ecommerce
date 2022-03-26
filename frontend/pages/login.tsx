202bf6c14ea347877c9a6c86bbcc11bf83f37c6a
Atechchatterjee <atechchatterjeee@gmail.com>
2022-03-26 22:53:46 +0000

    style: change positioning of login component

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -24,71 +24,73 @@ const Login: NextPage = () => {
   const [width] = useWindowDimensions();
 
   return (
-    <>
+    <Box position="relative" h="100vh" w="100%">
       <Header />
       <CustomContainer
         height="35em"
         borderRadius="xl"
         marginBottom="7%"
         width="100%"
-        position="relative"
+        position="absolute"
+        display="flex"
+        alignContent="center"
+        top="12%"
+        left="31%"
       >
-        <Center color="gray" marginTop="7vh" position="relative">
-          <Box
-            padding={width >= 500 ? "0 10% 0% 10%" : "0"}
-            marginTop="3em"
-            width="100%"
-            height="60%"
+        <Box
+          padding={width >= 500 ? "0 10% 0% 10%" : "0"}
+          marginTop="3em"
+          width="100%"
+          height="100%"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Tabs
+            align="center"
+            isFitted={false}
+            variant="line"
+            isLazy
             position="relative"
+            defaultIndex={1}
           >
-            <Tabs
-              align="center"
-              isFitted={false}
-              variant="line"
-              isLazy
-              position="relative"
-              defaultIndex={1}
-            >
-              <TabList>
-                <Tab
-                  _focus={{
-                    outline: "none",
-                  }}
-                  _selected={{ color: "primary.500" }}
-                >
-                  <Heading as="h2" size="md" isTruncated fontFamily="Sora">
-                    Sign Up
-                  </Heading>
-                </Tab>
-                <Tab
-                  _focus={{
-                    outline: "none",
-                  }}
-                  _selected={{ color: "primary.500" }}
-                >
-                  <Heading as="h2" size="md" isTruncated fontFamily="Sora">
-                    Sign In
-                  </Heading>
-                </Tab>
-              </TabList>
-              <TabPanels position="relative">
-                <TabPanel>
-                  <Fade in={true}>
-                    <SignUp />
-                  </Fade>
-                </TabPanel>
-                <TabPanel>
-                  <Fade in={true}>
-                    <SignIn />
-                  </Fade>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-          </Box>
-        </Center>
+            <TabList>
+              <Tab
+                _focus={{
+                  outline: "none",
+                }}
+                _selected={{ color: "primary.500" }}
+              >
+                <Heading as="h2" size="md" isTruncated fontFamily="Sora">
+                  Sign Up
+                </Heading>
+              </Tab>
+              <Tab
+                _focus={{
+                  outline: "none",
+                }}
+                _selected={{ color: "primary.500" }}
+              >
+                <Heading as="h2" size="md" isTruncated fontFamily="Sora">
+                  Sign In
+                </Heading>
+              </Tab>
+            </TabList>
+            <TabPanels position="relative">
+              <TabPanel>
+                <Fade in={true}>
+                  <SignUp />
+                </Fade>
+              </TabPanel>
+              <TabPanel>
+                <Fade in={true}>
+                  <SignIn />
+                </Fade>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
       </CustomContainer>
-      <Footer />
-    </>
+    </Box>
   );
 };
 
