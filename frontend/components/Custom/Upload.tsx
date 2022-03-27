ca73360f66948e804d808583d36aee7be3e1cc8a
Atechchatterjee <atechchatterjeee@gmail.com>
2022-03-27 22:15:18 +0000

    chore: remove unnecessary imports

--- frontend/components/Custom/Upload.tsx
+++ frontend/components/Custom/Upload.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useContext } from "react";
 import { UploadContext } from "../../context/UploadContext";
-import { Input } from "@chakra-ui/react";
 import { CustomField } from "./CustomField";
 
 interface Props {
