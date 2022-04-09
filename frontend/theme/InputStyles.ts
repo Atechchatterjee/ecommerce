78044f56e1bf4a787ebc430ea65b2190dccd962b
Atechchatterjee <atechchatterjeee@gmail.com>
2022-04-09 22:36:07 +0000

    style: change font color & family

--- frontend/theme/InputStyles.ts
+++ frontend/theme/InputStyles.ts
@@ -2,7 +2,8 @@ export default {
   baseStyle: {
     field: {
       focusBorderColor: "secondary.200",
-      color: "primary.500",
+      color: "gray.600",
+      fontFamily: "Sora"
     }, 
     defaultProps: {
       focusBorderColor: "secondary.200",
