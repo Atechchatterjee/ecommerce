7280ed8076b9d25a5987b0d5190afd321e709a16
Atechchatterjee <atechchatterjeee@gmail.com>
2022-04-11 22:48:46 +0530

    feat: add transition

--- frontend/components/Custom/CustomSlider.tsx
+++ frontend/components/Custom/CustomSlider.tsx
@@ -15,10 +15,10 @@ interface CustomSliderProps extends SliderProps {
 const CustomSlider = ({ icon, children, ...props }: CustomSliderProps) => (
   <Slider defaultValue={60} min={0} max={3} step={1} {...props}>
     {children}
-    <SliderTrack bg="gray.300" opacity="0.8">
+    <SliderTrack bg="gray.300" opacity="0.8" transition="all ease-in-out 0.2s">
       {icon ? icon : <SliderFilledTrack bg="secondary.200" />}
     </SliderTrack>
-    <SliderThumb boxSize={8}>
+    <SliderThumb boxSize={8} transition="all ease-in-out 0.2s">
       <Box color="secondary.200" as={FaShoppingCart} size={15} />
     </SliderThumb>
   </Slider>
