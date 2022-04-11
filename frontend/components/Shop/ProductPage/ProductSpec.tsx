735cad6a17ac0a2cc482ca8da0961962e09c28e5
Atechchatterjee <atechchatterjeee@gmail.com>
2022-04-11 22:49:34 +0530

    refactor: seperate the product update form

--- frontend/components/Shop/ProductPage/ProductSpec.tsx
+++ frontend/components/Shop/ProductPage/ProductSpec.tsx
@@ -1,5 +1,13 @@
-import React, { useState, useContext, useEffect } from "react";
-import { Box, Button, Container, Text, Image } from "@chakra-ui/react";
+import React, { useState, useContext, useEffect, useReducer } from "react";
+import {
+  Spinner,
+  Box,
+  Button,
+  Container,
+  Text,
+  Image,
+  Flex,
+} from "@chakra-ui/react";
 import axios from "axios";
 import constants from "../../../util/Constants";
 import SpecificationTable from "./SpecificationTable";
@@ -12,6 +20,96 @@ import { getProductInfo } from "../../../util/ProductInfo";
 import CustomContainer from "../../Custom/CustomContainer";
 import { CustomField } from "../../Custom/CustomField";
 
+const productValueReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "set-product-name":
+      return { ...state, name: action.payload };
+    case "set-product-description":
+      return { ...state, description: action.payload };
+    case "set-product-price":
+      return { ...state, price: action.payload };
+    default:
+      return state;
+  }
+};
+
+const UpdateProductValueForm = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const { productInfo } = useContext(ProductInfoContext);
+  const [product, setProduct] = productInfo;
+
+  const [productValues, dispatchProductValues] = useReducer(
+    productValueReducer,
+    {
+      name: product.name,
+      description: product.description,
+      price: product.price,
+    }
+  );
+
+  const updateProduct = () => {
+    setLoading(true);
+    axios
+      .post(
+        `${constants.url}/shop/updateproduct/`,
+        {
+          id: product.id,
+          ...productValues,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then(() => {
+        getProductInfo(product.id)
+          .then((newProduct) => {
+            setTimeout(() => setLoading(false), 500);
+            setProduct(newProduct);
+          })
+          .catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleChange = (e: any, type: string) => {
+    dispatchProductValues({
+      type,
+      payload: e.target.value,
+    });
+  };
+
+  return (
+    <Flex flexDirection="column" gridGap={5} w="100%" mb="5%">
+      <CustomField
+        w="100%"
+        label="Product Name"
+        value={productValues.name}
+        onChange={(e: any) => handleChange(e, "set-product-name")}
+      />
+      <CustomField
+        w="100%"
+        h="10em"
+        padding="2%"
+        as="textarea"
+        label="Product Description"
+        value={productValues.description}
+        onChange={(e: any) => handleChange(e, "set-product-description")}
+      />
+      <CustomField
+        w="100%"
+        label="Product Price"
+        value={productValues.price}
+        type="number"
+        onChange={(e: any) => handleChange(e, "set-product-price")}
+      />
+      <Button mt="3%" mb="5%" variant="primarySolid" onClick={updateProduct}>
+        {loading ? <Spinner size="sm" /> : "Save"}
+      </Button>
+    </Flex>
+  );
+};
+
 const ProductSpec: React.FC = () => {
   const [specTableHeading, setSpecTableHeading] = useState<any[]>([]);
   const [tableExists, setTableExists] = useState<boolean>(
@@ -21,10 +119,11 @@ const ProductSpec: React.FC = () => {
   const [modifyAddRowModal, setModifyAddRowModal] = useState<boolean>(false);
   const [isOpenOptionModal, setIsOpenOptionModal] = useState<boolean>(false);
   const [uploadedImages, setUploadedImages] = useState<File[]>([]);
-  const { productInfo } = useContext(ProductInfoContext);
-  const [product, setProduct] = productInfo;
   const [clearUploadedFiles, setClearUploadedFiles] = useState<boolean>(false);
   const [selectedImage, setSelectedImage] = useState<number>(0);
+
+  const { productInfo } = useContext(ProductInfoContext);
+  const [product, setProduct] = productInfo;
 
   const createTableHeading = () => {
     if (specTableHeading.length === 0)
@@ -55,18 +154,20 @@ const ProductSpec: React.FC = () => {
   const CreateSpecificationTableBtn: React.FC = () => {
     if (!tableExists)
       return (
-        <Button
-          marginTop="1.5em"
-          marginLeft="4em"
-          width="30em"
-          variant="primarySolid"
-          onClick={() => {
-            createTableHeading();
-            createTable();
-          }}
-        >
-          Create Specification Table
-        </Button>
+        !tableExists && (
+          <Button
+            marginTop="1.5em"
+            marginLeft="4em"
+            width="30em"
+            variant="primarySolid"
+            onClick={() => {
+              createTableHeading();
+              createTable();
+            }}
+          >
+            Create Specification Table
+          </Button>
+        )
       );
     else return <></>;
   };
@@ -121,7 +222,7 @@ const ProductSpec: React.FC = () => {
                   0,
                   constants?.url.lastIndexOf("/")
                 )}${product.image[selectedImage].image}
-                `}
+                    `}
                 width="100%"
                 height="30em"
               />
@@ -140,6 +241,7 @@ const ProductSpec: React.FC = () => {
             marginTop="10%"
             marginLeft="-3%"
             width="110%"
+            height="14vh"
             clearUpload={[clearUploadedFiles, setClearUploadedFiles]}
             onFileUpload={(files) => {
               console.log({ files });
@@ -169,35 +271,7 @@ const ProductSpec: React.FC = () => {
           </Button>
         </Container>
         <Container float="left" marginTop="2em" marginLeft="5em" width="40em">
-          <CustomField
-            w="95%"
-            mt="2%"
-            label="Product Name"
-            value={product.name}
-            placeholder="Product Name"
-            isRequired={false}
-          />
-          <CustomField
-            w="95%"
-            mt="4%"
-            padding="2%"
-            h="10em"
-            label="Product Description"
-            value={product.description}
-            placeholder="Product Description"
-            isRequired={false}
-            as="textarea"
-          />
-          <CustomField
-            w="95%"
-            mt="2%"
-            mb="5%"
-            label="Product Price"
-            value={product.price}
-            type="number"
-            isRequired={false}
-            placeholder="Product Price"
-          />
+          <UpdateProductValueForm />
           <SpecificationTable />
           <OptionsTable
             product={product}
