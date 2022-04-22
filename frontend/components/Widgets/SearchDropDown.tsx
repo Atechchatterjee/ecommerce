186c8013497e99ee45ed636b562745eb9695f8e0
Atechchatterjee <atechchatterjeee@gmail.com>
2022-04-22 22:08:13 +0530

    feat: use key press hook to move selection in the list

--- frontend/components/Widgets/SearchDropDown.tsx
+++ frontend/components/Widgets/SearchDropDown.tsx
@@ -3,21 +3,44 @@ import CustomContainer from "../Custom/CustomContainer";
 import { ContainerProps, Box } from "@chakra-ui/react";
 import { scrollBarStyle } from "../../util/ScrollBarStyle";
 import { AnimatePresence, motion } from "framer-motion";
+import { useKeyPress } from "../../hooks/useKeyPress";
 
 interface SearchDropDownProps extends ContainerProps {
   items: any[];
 }
 
 const SearchDropDown = ({ items, ...props }: SearchDropDownProps) => {
-  const [selectedItem, setSelectedItem] = useState<any>(items[0].item);
+  const [selectedItem, setSelectedItem] = useState<any>(
+    items[0] && items[0].item
+  );
+  const [lastItemSelected, setLastItemSelected] = useState<number>(0);
+  const downPress = useKeyPress("ArrowDown");
+  const upPress = useKeyPress("ArrowUp");
 
   const handleSelectItem = (item: any) => {
     setSelectedItem(item);
   };
 
   useEffect(() => {
-    setSelectedItem(items[0].item);
+    if (items[0]) setSelectedItem(items[0].item);
   }, [items]);
+
+  useEffect(() => {
+    // move selection to the next item in the list
+    if (items[lastItemSelected] && downPress) {
+      if (lastItemSelected < items.length - 1) {
+        setSelectedItem(items[lastItemSelected + 1].item);
+        setLastItemSelected(lastItemSelected + 1);
+      }
+    }
+    // move selection to the previous item in the list
+    if (items[lastItemSelected] && upPress) {
+      if (lastItemSelected > 0) {
+        setSelectedItem(items[lastItemSelected - 1].item);
+        setLastItemSelected(lastItemSelected - 1);
+      }
+    }
+  }, [downPress, upPress]);
 
   return (
     <AnimatePresence>
@@ -35,6 +58,9 @@ const SearchDropDown = ({ items, ...props }: SearchDropDownProps) => {
           overflow="scroll"
           sx={scrollBarStyle({ color: "primary", borderRadius: "lg" })}
           transition="all ease-in-out 0.5s"
+          onKeyDown={(e: any) => {
+            alert(e.keyCode);
+          }}
           {...props}
         >
           {items.map(({ item }, indx) => (
