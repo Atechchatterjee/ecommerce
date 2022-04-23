376eb8b7eff6d637391c97871712449d10779bb0
Atechchatterjee <atechchatterjeee@gmail.com>
2022-04-23 21:41:44 +0530

    feat: use escape key to hide drop down

--- frontend/components/Widgets/CategorySearch.tsx
+++ frontend/components/Widgets/CategorySearch.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CustomField } from "../Custom/CustomField";
 import { Box, InputProps } from "@chakra-ui/react";
 import { CategoryTree } from "../../util/Tree";
 import { searchCategory } from "../../util/SearchCategory";
 import SearchDropDown from "./SearchDropDown";
+import { useKeyPress } from "../../hooks/useKeyPress";
 
 interface CategoryTreeProps extends InputProps {
   categoryTree: CategoryTree;
@@ -12,26 +13,48 @@ interface CategoryTreeProps extends InputProps {
 const CategorySearch = ({ categoryTree, ...props }: CategoryTreeProps) => {
   const [searchPhrase, setSearchPhrase] = useState<string>("");
   const [searchedCategories, setSearchedCategories] = useState<any[]>([]);
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+  const escapeKeyPress = useKeyPress("Escape");
 
-  const handleChange = (e: any) => {
+  const handleSearch = (e: any) => {
     setSearchPhrase(e.target.value);
+    setShowDropDown(e.target.value !== "");
     setSearchedCategories(searchCategory(categoryTree, e.target.value, 5));
   };
 
+  const handleDropDownSelection = (item: any, enter?: boolean) => {
+    if (item) setSearchPhrase(item.name);
+    if (enter) setShowDropDown(false);
+  };
+
+  useEffect(() => {
+    if (escapeKeyPress) {
+      setShowDropDown(false);
+    }
+  }, [escapeKeyPress]);
+
   return (
-    <Box width="100%" justifyContent="center" padding="1em" position="relative">
+    <Box
+      width="100%"
+      justifyContent="center"
+      padding="2em 3em 0em 3em"
+      position="relative"
+    >
       <CustomField
+        size="lg"
+        borderRadius="md"
         value={searchPhrase}
         placeholder="Search Categories"
-        onChange={handleChange}
+        onChange={handleSearch}
         {...props}
       />
-      {searchPhrase && (
+      {showDropDown && (
         <SearchDropDown
           items={searchedCategories}
-          width="95%"
+          width="100%"
           mt="2%"
           borderRadius="lg"
+          selectedCb={handleDropDownSelection}
         />
       )}
     </Box>
