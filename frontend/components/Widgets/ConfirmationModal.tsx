f343e2b4477617e5727a2e79e9a18eaeaece642d
Atechchatterjee <atechchatterjeee@gmail.com>
2022-04-27 22:42:03 +0530

    feat: modal to confirm a certain action

---
+++ frontend/components/Widgets/ConfirmationModal.tsx
@@ -0,0 +1,60 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  ModalProps,
+} from "@chakra-ui/react";
+import React, { Dispatch, SetStateAction } from "react";
+
+interface ConfirmationModalProps {
+  open: boolean;
+  setOpen: Function | Dispatch<SetStateAction<boolean>>;
+  modalTitle?: string;
+  modalBody?: any;
+  buttonText?: string;
+  children?: any;
+  confirmCb?: Function;
+}
+
+const ConfirmationModal = ({
+  buttonText,
+  modalTitle,
+  modalBody,
+  open,
+  setOpen,
+  confirmCb,
+  children,
+}: ConfirmationModalProps) => {
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Modal onClose={handleClose} isOpen={open}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{modalTitle || "Confirm"}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{modalBody}</ModalBody>
+          <ModalFooter>
+            <Button
+              onClick={() => {
+                handleClose();
+                if (confirmCb) confirmCb();
+              }}
+            >
+              {buttonText || "Confirm"}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      {children}
+    </>
+  );
+};
+
+export default ConfirmationModal;
