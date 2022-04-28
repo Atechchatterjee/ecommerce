ff529885f41c19984b32ed45e8285bcd8d57e26b
Atechchatterjee <atechchatterjeee@gmail.com>
2022-04-28 22:24:38 +0530

    chore: formatting change

--- frontend/services/CategoryService.ts
+++ frontend/services/CategoryService.ts
@@ -5,15 +5,19 @@ import constants from "../util/Constants";
 
 export const createCategory = async (data: Category): Promise<void> => {
   try {
-    if(data.category_name.length === 0) return;
-    await axios.post(`${constants.url}/shop/createcategory/`, data, {withCredentials: true});
+    if (data.category_name.length === 0) return;
+    await axios.post(`${constants.url}/shop/createcategory/`, data, {
+      withCredentials: true,
+    });
     return Promise.resolve();
   } catch (err) {
     return Promise.reject(err);
   }
 };
 
-export const deleteCategory = async (categoryId: CategoryNode): Promise<void> => {
+export const deleteCategory = async (
+  categoryId: CategoryNode
+): Promise<void> => {
   axios
     .delete(`${constants.url}/shop/delete-category/${categoryId}/`, {
       withCredentials: true,
