aa647e221b010afe791e2c60c95407f007dba968
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-01 08:57:33 +0530

    chore: conditionally change the backend url

--- frontend/util/Constants.ts
+++ frontend/util/Constants.ts
@@ -1,8 +1,8 @@
 export default {
   url:
-    // process.env.NODE_ENV === 'production'
-    //   ? process.env.NEXT_PUBLIC_PROD_URL
-      // :
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_PROD_URL
+      :
        process.env.NEXT_PUBLIC_URL,
   googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
   fallbackURL: "https://newhorizon-bsh.s3.ap-south-1.amazonaws.com/nhengineering/bsh/wp-content/uploads/2020/01/17113522/default-image.png"
