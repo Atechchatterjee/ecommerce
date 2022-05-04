3462008142a510018989b0526d1906800f13a11e
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-04 21:34:05 +0530

    feat: use a custom select unit menu

--- frontend/components/Admin/AddProduct.tsx
+++ frontend/components/Admin/AddProduct.tsx
@@ -3,17 +3,15 @@ import {
   Center,
   Heading,
   ContainerProps,
-  Input,
   Button,
   useToast,
-  Textarea,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
-  NumberInputField,
   NumberInputStepper,
+  Flex,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import constants from "../../util/Constants";
 import SelectCategory from "./SelectCategory";
@@ -21,6 +19,9 @@ import { Formik } from "formik";
 import DragUpload from "../Custom/DragUpload";
 import { CustomField } from "../Custom/CustomField";
 import CustomContainer from "../Custom/CustomContainer";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { fetchUnits } from "../../services/UnitService";
+import SelectUnitMenu from "../Widgets/SelectUnitMenu";
 
 interface ProductData {
   productName: string;
@@ -28,12 +29,14 @@ interface ProductData {
   productPrice: string;
   categoryId: number | null;
   productImages: File[] | null;
+  unitId: number;
 }
 
 const createProduct = async ({
   productName,
   productDescription,
   productPrice,
+  unitId,
   categoryId,
   productImages,
 }: ProductData): Promise<void> => {
@@ -42,6 +45,7 @@ const createProduct = async ({
   formData.append("productName", productName);
   formData.append("productDescription", productDescription);
   formData.append("productPrice", productPrice);
+  formData.append("unitId", unitId.toString());
 
   if (typeof categoryId === "number")
     formData.append("categoryId", categoryId.toString());
@@ -67,8 +71,35 @@ const AddProduct = (props: ContainerProps) => {
   const [categoryId, setCategoryId] = useState<number | null>(null);
   const [productImages, setProductImages] = useState<File[] | null>(null);
   const [clearUploadFiles, setClearUploadFiles] = useState<boolean>(false);
+  const [allUnits, setAllUnits] = useState<any[]>([]);
+  const [selectedUnit, setSelectedUnit] = useState<any>();
 
   const toast = useToast();
+
+  const showSuccessToast = (values?: any) => {
+    toast({
+      title: "Created",
+      description: !values
+        ? "Product was created"
+        : `Product: '${values.productName}' has been created`,
+      duration: 2000,
+      isClosable: true,
+      status: "success",
+    });
+  };
+  const showErrorToast = (errorMsg: string) => {
+    toast({
+      title: "Sorry for the inconvenience",
+      description: errorMsg,
+      duration: 2000,
+      isClosable: true,
+      status: "error",
+    });
+  };
+
+  useEffect(() => {
+    fetchUnits().then((allUnits) => setAllUnits(allUnits));
+  }, []);
 
   return (
     <Center marginBottom="2%">
@@ -96,31 +127,25 @@ const AddProduct = (props: ContainerProps) => {
               productPrice: "",
             }}
             onSubmit={(values: any, { setSubmitting, resetForm }) => {
-              setSubmitting(true);
-              values["categoryId"] = categoryId;
-              values["productImages"] = productImages;
-              console.log({ values });
-              createProduct(values)
-                .then(() => {
-                  toast({
-                    title: "Created",
-                    description: `Product: '${values.productName}' has been created`,
-                    duration: 2000,
-                    isClosable: true,
-                    status: "success",
+              if (!categoryId) {
+                showErrorToast("Please select a category");
+                setSubmitting(false);
+              } else {
+                setSubmitting(true);
+                values["categoryId"] = categoryId;
+                values["productImages"] = productImages;
+                values["unitId"] = selectedUnit ? selectedUnit.unit_id : -1;
+                createProduct(values)
+                  .then(() => {
+                    showSuccessToast(values);
+                    setSubmitting(false);
+                    setTimeout(() => window.location.reload());
+                  })
+                  .catch(() => {
+                    showErrorToast("Product could not be created!");
+                    setSubmitting(false);
                   });
-                  setSubmitting(false);
-                })
-                .catch(() => {
-                  toast({
-                    title: "Sorry for the inconvenience",
-                    description: `Product: '${values.productName}' could not be created`,
-                    duration: 2000,
-                    isClosable: true,
-                    status: "error",
-                  });
-                  setSubmitting(false);
-                });
+              }
             }}
           >
             {({
@@ -131,76 +156,87 @@ const AddProduct = (props: ContainerProps) => {
               isSubmitting,
             }) => (
               <form onSubmit={handleSubmit}>
-                <CustomField
-                  name="productName"
-                  placeholder="Product Name"
-                  value={values.productName}
-                  borderRadius="sm"
-                  onChange={handleChange}
-                  top="1em"
-                />
-                <CustomField
-                  name="productDescription"
-                  placeholder="Product Description"
-                  value={values.productDescription}
-                  borderRadius="sm"
-                  onChange={handleChange}
-                  padding="0.5em"
-                  minHeight="10em"
-                  top="3em"
-                  as="textarea"
-                />
-                <NumberInput
-                  top="4em"
-                  precision={2}
-                  step={1}
-                  borderRadius="lg"
-                  onReset={() => alert("reseting")}
-                >
+                <Flex flexDirection="column" gridGap={8}>
                   <CustomField
-                    value={values.productPrice}
-                    placeholder="Prodcut Price"
+                    name="productName"
+                    placeholder="Product Name"
+                    value={values.productName}
                     borderRadius="sm"
-                    name="productPrice"
                     onChange={handleChange}
-                    onBlur={handleBlur}
                   />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-                <SelectCategory
-                  marginTop="6em"
-                  text="Category"
-                  width="100%"
-                  borderRadius="sm"
-                  selectCb={({ selectedCategory }) => {
-                    if (selectedCategory?.val)
-                      setCategoryId(selectedCategory?.val.id);
-                  }}
-                />
-                <DragUpload
-                  marginTop="2em"
-                  marginLeft="-1em"
-                  width="34em"
-                  onFileUpload={(files) => {
-                    console.log({ files });
-                    setProductImages(files);
-                  }}
-                  clearUpload={[clearUploadFiles, setClearUploadFiles]}
-                />
-                <Button
-                  borderRadius="sm"
-                  variant="primarySolid"
-                  size="md"
-                  top="3em"
-                  width="full"
-                  type="submit"
-                  isLoading={isSubmitting}
-                >
-                  Submit
-                </Button>
+                  <CustomField
+                    name="productDescription"
+                    placeholder="Product Description"
+                    value={values.productDescription}
+                    borderRadius="sm"
+                    onChange={handleChange}
+                    padding="0.5em"
+                    minHeight="10em"
+                    as="textarea"
+                  />
+                  <Flex flexDirection="row" position="relative" gridGap={3}>
+                    <NumberInput
+                      precision={2}
+                      step={1}
+                      borderRadius="lg"
+                      onReset={() => alert("reseting")}
+                      w="100%"
+                    >
+                      <CustomField
+                        value={values.productPrice}
+                        placeholder="Prodcut Price"
+                        borderRadius="sm"
+                        name="productPrice"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </NumberInput>
+                    <SelectUnitMenu
+                      allUnits={allUnits}
+                      selectedUnit={selectedUnit}
+                      setSelectedUnit={setSelectedUnit}
+                    />
+                  </Flex>
+                  <SelectCategory
+                    text="Select Category"
+                    width="100%"
+                    borderRadius="sm"
+                    bgColor={categoryId ? "secondary.200" : ""}
+                    _hover={
+                      categoryId
+                        ? { bgColor: "secondary.200", opacity: "0.8" }
+                        : { opacity: "0.9" }
+                    }
+                    size="lg"
+                    selectCb={({ selectedCategory }) => {
+                      if (selectedCategory?.val)
+                        setCategoryId(selectedCategory?.val.id);
+                    }}
+                  />
+                  <DragUpload
+                    marginLeft="-1em"
+                    width="34em"
+                    onFileUpload={(files) => {
+                      console.log({ files });
+                      setProductImages(files);
+                    }}
+                    clearUpload={[clearUploadFiles, setClearUploadFiles]}
+                  />
+                  <Button
+                    borderRadius="sm"
+                    variant="primarySolid"
+                    size="lg"
+                    width="full"
+                    type="submit"
+                    isLoading={isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                </Flex>
               </form>
             )}
           </Formik>
