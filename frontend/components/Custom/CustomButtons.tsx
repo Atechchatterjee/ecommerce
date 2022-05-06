734ec2e078fd30c0e199982ea0baf65449be741e
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-06 22:09:28 +0530

    feat: store frequently used buttons

---
+++ frontend/components/Custom/CustomButtons.tsx
@@ -0,0 +1,40 @@
+import { Button, ButtonProps } from "@chakra-ui/react";
+import { FaTrash } from "react-icons/fa";
+import { FiPlus } from "react-icons/fi";
+import { MdDone } from "react-icons/md";
+
+interface CustomButtonProps extends ButtonProps {
+  iconSize?: string | number;
+}
+
+export const SaveButton = ({ iconSize, ...props }: CustomButtonProps) => {
+  return (
+    <Button variant="primarySolid" padding="0.8em" {...props}>
+      <MdDone size={iconSize || 20} />
+    </Button>
+  );
+};
+
+export const DeleteButton = ({ iconSize, ...props }: CustomButtonProps) => {
+  return (
+    <Button variant="primarySolid" {...props}>
+      <FaTrash size={iconSize || 17} />
+    </Button>
+  );
+};
+
+export const AddButton = ({ iconSize, ...props }: CustomButtonProps) => {
+  return (
+    <Button
+      width="8%"
+      mb="0.5em"
+      padding="0.8em"
+      variant="primarySolid"
+      {...props}
+    >
+      <FiPlus size={iconSize || 30} />
+    </Button>
+  );
+};
+
+export default Button;
