e19e29096a11a93a9e685f27c2f94a0dd92fd686
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-07 22:34:21 +0530

    feat: seperated all api end points for products

---
+++ frontend/services/ProductService.ts
@@ -0,0 +1,30 @@
+import api from "../util/AxiosApi"
+
+export const addProduct = async (formData: FormData) => {
+  const res = await api.post("/shop/createProduct/", formData, {withCredentials: true});
+  return Promise.resolve(res);
+}
+
+export const updateProduct = async (data: any) => {
+  const res = await api.post('/shop/updateProduct/', data, {withCredentials: true});
+  if (res.status === 200) return Promise.resolve(res);
+}
+
+// TODO: change post method to get
+export const getProductInfo = async (productId: any) => {
+  const res = await api.post('/shop/getproduct/', {id: productId})
+  if(res) {
+    // change product_id field to id
+    let modifiedProduct = {
+      ...res.data.product,
+      id: res.data.product.product_id,
+    };
+    delete modifiedProduct["product_id"];
+    return Promise.resolve(modifiedProduct);
+  }
+}
+
+export const addProductImages = async (formData: FormData) => {
+  const res = await api.post('/shop/addproductimages/', formData, {withCredentials: true});
+  if (res) return Promise.resolve(res);
+}
