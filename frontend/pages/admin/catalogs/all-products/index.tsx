f999ef2e6ab172ed9b0e3f78a230de55517e99b4
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-12 23:23:05 +0530

    chore: remove imports

--- frontend/pages/admin/catalogs/all-products/index.tsx
+++ frontend/pages/admin/catalogs/all-products/index.tsx
@@ -4,7 +4,6 @@ import { Center, Container } from "@chakra-ui/react";
 import AllProducts from "../../../../components/Admin/AllProducts";
 import Navbar from "../../../../components/Admin/Navbar";
 import WithAuth from "../../../../util/WithAuth";
-import { Box } from "@chakra-ui/react";
 
 const Product: NextPage = () => {
   return (
