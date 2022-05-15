81ad4a690077caecaec54e5de41507acc597f040
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-15 23:03:23 +0530

    feat: a view to send emails

--- backend/src/administrator/views.py
+++ backend/src/administrator/views.py
@@ -1,7 +1,9 @@
+from operator import itemgetter
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from authentication.models import User
+from django.core.mail import send_mail
 
 
 @api_view(['GET'])
@@ -22,3 +24,12 @@ def get_all_users(request):
     return Response({"all_users": users}, status=status.HTTP_200_OK)
   else:
       return Response(status=status.HTTP_403_FORBIDDEN)
+
+@api_view(['POST'])
+def send_email(request):
+  (msg_subject, msg_body) = itemgetter('msgSubject', 'msgBody')(request.data)
+  try:
+    send_mail(msg_subject, msg_body, 'atechchatterjeee@gmail.com', ['peechatterjee@gmail.com'], fail_silently=False)
+    return Response(status=status.HTTP_200_OK)
+  except: 
+      return Response(status=status.HTTP_400_BAD_REQUEST)
