6b7dedf1eb9951216f45e4a473625599a9826bdc
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-16 22:27:44 +0530

    refactor:  split product spec & renamed

---
+++ frontend/components/Shop/ProductPage/ProductSpecification.tsx
@@ -0,0 +1,208 @@
+import React, { useState, useContext } from "react";
+import { Button, Container, Text, Image, Flex, Box } from "@chakra-ui/react";
+import axios from "axios";
+import constants from "../../../util/Constants";
+import SpecificationTable from "./SpecificationTable";
+import { SpecTableContext } from "../../../context/SpecTableContext";
+import ImageGallery from "../Product/ImageGallery";
+import DragUpload from "../../Custom/DragUpload";
+import { ProductInfoContext } from "../../../context/ProductInfoContext";
+import { getProductInfo } from "../../../util/ProductInfo";
+import CustomContainer from "../../Custom/CustomContainer";
+import OptionsTable from "../../Admin/OptionsTable";
+import api from "../../../util/AxiosApi";
+import UpdateProductValueForm from "./UpdateProductValueForm";
+
+const ProductSpec: React.FC = () => {
+  const [specTableHeading, setSpecTableHeading] = useState<any[]>([]);
+  const [tableExists, setTableExists] = useState<boolean>(
+    specTableHeading.length !== 0
+  );
+  const [openAddRowModal, setOpenAddRowModal] = useState<boolean>(false);
+  const [modifyAddRowModal, setModifyAddRowModal] = useState<boolean>(false);
+  const [uploadedImages, setUploadedImages] = useState<File[]>([]);
+  const [clearUploadedFiles, setClearUploadedFiles] = useState<boolean>(false);
+  const [selectedImage, setSelectedImage] = useState<number>(0);
+  const [dropDownStatus, setDropDownStatus] = useState<boolean>(false);
+
+  const { productInfo } = useContext(ProductInfoContext);
+  const [product, setProduct] = productInfo;
+
+  const createTableHeading = () => {
+    if (specTableHeading.length === 0)
+      setSpecTableHeading([
+        ...specTableHeading,
+        <Text key={specTableHeading.length + 1} fontWeight="bold">
+          Specification
+        </Text>,
+        <Text key={specTableHeading.length + 2} fontWeight="bold">
+          Details
+        </Text>,
+      ]);
+  };
+
+  const createTable = async () => {
+    await api.post(`/shop/createtable/${product.id}`, {
+      withCredentials: true,
+    });
+    setTableExists(true);
+  };
+
+  const CreateSpecificationTableBtn: React.FC = () => {
+    if (!tableExists)
+      return (
+        !tableExists && (
+          <Button
+            size="lg"
+            mb="2em"
+            width="100%"
+            variant="primarySolid"
+            onClick={() => {
+              createTableHeading();
+              createTable();
+            }}
+          >
+            Create Specification Table
+          </Button>
+        )
+      );
+    else return <></>;
+  };
+
+  const uploadAllImages = () => {
+    let formData = new FormData();
+    formData.append("productId", product.id.toString());
+    uploadedImages.forEach((img, indx) => {
+      formData.append(`file[${indx}]`, img);
+    });
+    axios
+      .post(`${constants.url}/shop/addproductimages/`, formData, {
+        withCredentials: true,
+      })
+      .then(() => {
+        getProductInfo(product.id)
+          .then((newProduct) => {
+            setProduct(newProduct);
+          })
+          .catch((err) => console.error(err));
+        setClearUploadedFiles(true);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  if (product)
+    return (
+      <SpecTableContext.Provider
+        value={{
+          headings: [specTableHeading, setSpecTableHeading],
+          tableExist: [tableExists, setTableExists],
+          openRowModal: [openAddRowModal, setOpenAddRowModal],
+          modifyRowModal: [modifyAddRowModal, setModifyAddRowModal],
+        }}
+      >
+        <Flex
+          marginLeft="12em"
+          position="absolute"
+          flexDirection="row"
+          flexWrap="wrap"
+          gridGap="100"
+          pb="5%"
+        >
+          <Container margin="0" marginTop="2em" height="110vh">
+            {product && (
+              <CustomContainer
+                borderRadius="2xl"
+                width="38em"
+                height="80%"
+                maxHeight="38em"
+                padding="5%"
+                position="relative"
+                interactive
+                transition="all ease-in-out 0.5s"
+              >
+                {product.image.map((curImg, indx) => (
+                  <Image
+                    position="absolute"
+                    key={indx}
+                    objectFit="contain"
+                    src={`${constants.url?.substring(
+                      0,
+                      constants?.url.lastIndexOf("/")
+                    )}${curImg.image}
+                    `}
+                    opacity={
+                      curImg.image === product.image[selectedImage].image
+                        ? 1
+                        : 0
+                    }
+                    width="90%"
+                    height="30em"
+                    transition="all ease-in-out 0.5s"
+                  />
+                ))}
+              </CustomContainer>
+            )}
+            <ImageGallery
+              marginTop="2em"
+              width="100%"
+              selectCb={(indx) => {
+                setSelectedImage(indx);
+              }}
+              allowEdit
+            />
+            <DragUpload
+              marginTop="10%"
+              marginLeft="-3%"
+              width="106%"
+              height="14vh"
+              clearUpload={[clearUploadedFiles, setClearUploadedFiles]}
+              onFileUpload={(files) => {
+                console.log({ files });
+                if (files.length !== 0) {
+                  setUploadedImages(files);
+                }
+              }}
+            />
+            <Button
+              variant="primarySolid"
+              margin="2em 0"
+              size="lg"
+              width="100%"
+              onClick={uploadAllImages}
+            >
+              Upload Additional Images
+            </Button>
+            <CreateSpecificationTableBtn />
+          </Container>
+          <Container marginTop="2em" width="100%" marginLeft="0">
+            <CustomContainer
+              padding="2rem 2rem 0.01rem 2rem"
+              width="120%"
+              borderRadius="lg"
+            >
+              <UpdateProductValueForm
+                getDropDownStatus={(status: any) => {
+                  setDropDownStatus(status);
+                }}
+              />
+            </CustomContainer>
+            <Box
+              position="relative"
+              zIndex={dropDownStatus ? "-1" : "11"}
+              w="110%"
+              mt="2.5rem"
+              ml="-1rem"
+            >
+              <SpecificationTable />
+              <OptionsTable mt="5%" borderRadius="lg" />
+            </Box>
+          </Container>
+        </Flex>
+      </SpecTableContext.Provider>
+    );
+  else return <></>;
+};
+
+export default ProductSpec;
---
+++ frontend/components/Shop/ProductPage/UpdateProductValueForm.tsx
@@ -0,0 +1,220 @@
+import { Text, Flex, Button } from "@chakra-ui/react";
+import { useState, useContext, useReducer, useEffect } from "react";
+import { FaSave } from "react-icons/fa";
+import { GSTSelectorContext } from "../../../context/GSTSelectorContext";
+import { ProductInfoContext } from "../../../context/ProductInfoContext";
+import {
+  updateProduct,
+  getProductInfo,
+} from "../../../services/ProductService";
+import { fetchUnits } from "../../../services/UnitService";
+import {
+  CategoryTree,
+  CategoryNode,
+  convertToCustomTree,
+} from "../../../util/Tree";
+import SelectCategory, { getAllCategory } from "../../Admin/SelectCategory";
+import { CustomField } from "../../Custom/CustomField";
+import EnterPrice from "../../Widgets/EnterPrice";
+import GSTSelectorModal from "../../Widgets/GSTSelectorModal";
+import SelectUnitMenu from "../../Widgets/SelectUnitMenu";
+
+const productValueReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "set-product-name":
+      return { ...state, name: action.payload };
+    case "set-product-description":
+      return { ...state, description: action.payload };
+    case "set-product-price":
+      return { ...state, price: action.payload };
+    case "set-product-category":
+      return { ...state, category: action.payload };
+    case "set-product-gst":
+      return { ...state, gst: action.payload };
+    default:
+      return state;
+  }
+};
+
+interface UpdateProductValueFormProps {
+  getDropDownStatus?: (status: boolean) => void;
+}
+
+const UpdateProductValueForm = ({
+  getDropDownStatus,
+}: UpdateProductValueFormProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [allUnits, setAllUnits] = useState<any[]>([]);
+  const [selectedUnit, setSelectedUnit] = useState<any>();
+
+  const { productInfo } = useContext(ProductInfoContext);
+  const [product, setProduct] = productInfo;
+
+  const [gstSelectedRows, setGstSelectedRows] = useState<any>(product.gst);
+  const [productValues, dispatchProductValues] = useReducer(
+    productValueReducer,
+    {
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      category: product.category.category_id,
+      gst: gstSelectedRows,
+    }
+  );
+  const [customTree, setCustomTree] = useState<CategoryTree>();
+  const [selectedCategory, setSelectedCategory] = useState<CategoryNode | null>(
+    null
+  );
+  const [allPriceData, setAllPriceData] = useState<any[]>(product.price);
+  const [deletedProductPriceIndx, setDeleteProductPriceIndx] = useState<any[]>(
+    []
+  );
+
+  useEffect(() => {
+    getAllCategory().then((categories) => {
+      const customTree = convertToCustomTree(categories);
+      setCustomTree(customTree);
+      // finding the category that matches the product category
+      customTree.findNodeById(product.category, customTree.root, (category) => {
+        setSelectedCategory(category);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    dispatchProductValues({
+      type: "set-product-gst",
+      payload: gstSelectedRows,
+    });
+  }, [gstSelectedRows]);
+
+  const handleUpdateProduct = () => {
+    setLoading(true);
+    updateProduct({
+      id: product.id,
+      unit: selectedUnit?.unit_id,
+      ...productValues,
+      price: allPriceData.filter((price) => price.new),
+      deletedProductPriceIndx,
+    })
+      .then(() => {
+        getProductInfo(product.id)
+          .then((newProduct) => {
+            setTimeout(() => setLoading(false), 500);
+            setProduct(newProduct);
+          })
+          .catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleChange = (e: any, type: string) => {
+    dispatchProductValues({
+      type,
+      payload: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    fetchUnits().then((units) => setAllUnits(units));
+  }, []);
+
+  useEffect(() => {
+    console.log({ product });
+    if (product.unit) {
+      setSelectedUnit(product.unit);
+    }
+  }, [product]);
+
+  return (
+    <Flex
+      flexDirection="column"
+      gridGap={5}
+      mb="5%"
+      justifyContent="left"
+      alignContent="left"
+    >
+      <CustomField
+        w="100%"
+        label="Product Name"
+        value={productValues.name}
+        onChange={(e: any) => handleChange(e, "set-product-name")}
+      />
+      <CustomField
+        w="100%"
+        h="10em"
+        padding="2%"
+        as="textarea"
+        label="Product Description"
+        value={productValues.description}
+        onChange={(e: any) => handleChange(e, "set-product-description")}
+      />
+      <EnterPrice
+        {...{ allPriceData, setAllPriceData }}
+        deleteCb={(id: any) =>
+          setDeleteProductPriceIndx([...deletedProductPriceIndx, id])
+        }
+      />
+      {customTree ? (
+        <Flex flexDirection="row" gridGap={5} mt="2.1rem">
+          <SelectCategory
+            flex="1"
+            height="4.9vh"
+            text={product.category.category_name || "Select Category"}
+            variant="primaryOutline"
+            selectCb={({ selectedCategory }) => {
+              if (selectedCategory) {
+                setSelectedCategory(selectedCategory);
+                dispatchProductValues({
+                  type: "set-product-category",
+                  payload: selectedCategory.val.id,
+                });
+              }
+            }}
+          />
+          <GSTSelectorContext.Provider
+            value={{
+              selectedRows: gstSelectedRows,
+              setSelectedRows: setGstSelectedRows,
+            }}
+          >
+            <GSTSelectorModal
+              borderRadius="sm"
+              fontSize="md"
+              flex="1"
+              variant="primaryOutline"
+            />
+          </GSTSelectorContext.Provider>
+
+          <SelectUnitMenu
+            size="lg"
+            variant="primaryOutline"
+            borderRadius="sm"
+            fontSize="md"
+            allUnits={allUnits}
+            selectedUnit={selectedUnit}
+            setSelectedUnit={setSelectedUnit}
+          />
+        </Flex>
+      ) : (
+        <></>
+      )}
+      <Button
+        size="lg"
+        mt="3%"
+        mb="5%"
+        variant="primarySolid"
+        onClick={handleUpdateProduct}
+        position="unset"
+        isLoading={loading}
+      >
+        <Flex flexDirection="row" gridGap={3}>
+          <Text>Save</Text>
+          <FaSave />
+        </Flex>
+      </Button>
+    </Flex>
+  );
+};
+
+export default UpdateProductValueForm;
