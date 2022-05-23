8aeef58a53482ee0b1dc679e67c69fdb8cdd8fe2
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-23 22:58:57 +0530

    feat: reduce margin for mobile screens

--- frontend/components/Shop/AdminProductPage/ProductSpecification.tsx
+++ frontend/components/Shop/AdminProductPage/ProductSpecification.tsx
@@ -1,5 +1,13 @@
 import React, { useState, useContext } from "react";
-import { Button, Container, Text, Image, Flex, Box } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Text,
+  Image,
+  Flex,
+  Box,
+  useDimensions,
+} from "@chakra-ui/react";
 import constants from "../../../util/Constants";
 import SpecificationTable from "./SpecificationTable";
 import { SpecTableContext } from "../../../context/SpecTableContext";
@@ -12,6 +20,7 @@ import OptionsTable from "../../Admin/OptionsTable";
 import api from "../../../util/AxiosApi";
 import UpdateProductValueForm from "./UpdateProductValueForm";
 import { addProductImages } from "../../../services/ProductService";
+import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
 
 const ProductSpec: React.FC = () => {
   const [specTableHeading, setSpecTableHeading] = useState<any[]>([]);
@@ -23,10 +32,11 @@ const ProductSpec: React.FC = () => {
   const [uploadedImages, setUploadedImages] = useState<File[]>([]);
   const [clearUploadedFiles, setClearUploadedFiles] = useState<boolean>(false);
   const [selectedImage, setSelectedImage] = useState<number>(0);
-  const [dropDownStatus, setDropDownStatus] = useState<boolean>(false);
 
   const { productInfo } = useContext(ProductInfoContext);
   const [product, setProduct] = productInfo;
+
+  const [width] = useWindowDimensions();
 
   const createTableHeading = () => {
     if (specTableHeading.length === 0)
@@ -95,8 +105,8 @@ const ProductSpec: React.FC = () => {
         }}
       >
         <Flex
-          marginLeft="7%"
-          marginRight="7%"
+          marginLeft={width > 525 ? "7%" : "1%"}
+          marginRight={width > 525 ? "7%" : "1%"}
           justifyContent="center"
           alignContent="center"
           flexDirection="row"
@@ -118,7 +128,7 @@ const ProductSpec: React.FC = () => {
                   <Image
                     position="absolute"
                     key={indx}
-                    objectFit="contain"
+                    objectFit="scale-down"
                     src={`${constants.url?.substring(
                       0,
                       constants?.url.lastIndexOf("/")
@@ -158,7 +168,8 @@ const ProductSpec: React.FC = () => {
             />
             <Button
               variant="primarySolid"
-              margin="3em 0"
+              margin="3rem 0"
+              position="relative"
               size="lg"
               width="100%"
               onClick={uploadAllImages}
@@ -173,18 +184,9 @@ const ProductSpec: React.FC = () => {
               width="100%"
               borderRadius="lg"
             >
-              <UpdateProductValueForm
-                getDropDownStatus={(status: any) => {
-                  setDropDownStatus(status);
-                }}
-              />
+              <UpdateProductValueForm />
             </CustomContainer>
-            <Box
-              position="relative"
-              zIndex={dropDownStatus ? "-1" : "11"}
-              w="100%"
-              mt="2.5rem"
-            >
+            <Box position="relative" w="100%" mt="2.5rem">
               <SpecificationTable />
               <OptionsTable mt="5%" borderRadius="lg" />
             </Box>
