ffdac58ee7130e9675f74bd2409998362a1d688c
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-24 22:32:52 +0530

    feat: made admin login screen responsive

--- frontend/pages/admin/login.tsx
+++ frontend/pages/admin/login.tsx
@@ -7,6 +7,7 @@ import constants from "../../util/Constants";
 import axios from "axios";
 import Banner from "../../components/Layout/Banner";
 import CustomContainer from "../../components/Custom/CustomContainer";
+import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 
 interface FormData {
   email: string;
@@ -17,6 +18,9 @@ const AdminLogin: NextPage = () => {
   const formikRef = useRef<any>();
   const redirectURL = useRef<string>("/admin/catalogs/all-products");
   const [err, setErr] = useState<boolean>(false);
+  const [width] = useWindowDimensions();
+
+  const BREAK_POINTS = [850, 920];
 
   const adminAuth = (
     setSubmitting: (submit: boolean) => void,
@@ -41,63 +45,71 @@ const AdminLogin: NextPage = () => {
   return (
     <div>
       <Banner text="Admin Login" />
-      <CustomContainer height="23em" borderRadius="sm">
-        <Center color="gray" style={{ marginTop: "7vh" }}>
-          <Stack
-            direction={["row", "column"]}
-            spacing="35px"
-            style={{ width: "25em", marginTop: "3em" }}
+      <CustomContainer
+        height="22em"
+        borderRadius="sm"
+        w={width < BREAK_POINTS[0] ? "80vw" : "50vw"}
+        mt="7vh"
+      >
+        <Stack
+          direction={["row", "column"]}
+          spacing="35px"
+          padding={
+            width > BREAK_POINTS[1]
+              ? "2rem 5rem 4rem 5rem"
+              : "2rem 3rem 2rem 3rem"
+          }
+        >
+          <Formik
+            innerRef={formikRef}
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            onSubmit={(formData, { setSubmitting }) =>
+              adminAuth(setSubmitting, formData)
+            }
           >
-            <Formik
-              innerRef={formikRef}
-              initialValues={{
-                email: "",
-                password: "",
-              }}
-              onSubmit={(formData, { setSubmitting }) =>
-                adminAuth(setSubmitting, formData)
-              }
-            >
-              {({ handleSubmit, handleChange }) => {
-                return (
-                  <form onSubmit={handleSubmit}>
-                    <Box marginTop="1em">
-                      <CustomField
-                        placeholder="Admin Email"
-                        name="email"
-                        label="Admin Email Id"
-                        borderRadius="sm"
-                        onChange={handleChange}
-                        isInvalid={err}
-                      />
-                      <br />
-                      <CustomField
-                        placeholder="Admin Password"
-                        name="password"
-                        type="password"
-                        label="Admin Password"
-                        onChange={handleChange}
-                        borderRadius="sm"
-                        isInvalid={err}
-                      />
-                      <br />
-                      <Button
-                        marginTop="1em"
-                        type="submit"
-                        variant="primarySolid"
-                        width="100%"
-                        borderRadius="sm"
-                      >
-                        Submit
-                      </Button>
-                      <br />
-                    </Box>
-                  </form>
-                );
-              }}
-            </Formik>
-          </Stack>
-        </Center>
+            {({ handleSubmit, handleChange }) => {
+              return (
+                <form onSubmit={handleSubmit} style={{ width: "100%" }}>
+                  <Box marginTop="1em">
+                    <CustomField
+                      placeholder="Admin Email"
+                      name="email"
+                      label="Admin Email Id"
+                      borderRadius="sm"
+                      w="100%"
+                      onChange={handleChange}
+                      isInvalid={err}
+                    />
+                    <br />
+                    <CustomField
+                      placeholder="Admin Password"
+                      name="password"
+                      type="password"
+                      label="Admin Password"
+                      onChange={handleChange}
+                      borderRadius="sm"
+                      isInvalid={err}
+                    />
+                    <br />
+                    <Button
+                      marginTop="1em"
+                      type="submit"
+                      variant="primarySolid"
+                      width="100%"
+                      borderRadius="sm"
+                    >
+                      Submit
+                    </Button>
+                    <br />
+                  </Box>
+                </form>
+              );
+            }}
+          </Formik>
+        </Stack>
       </CustomContainer>
     </div>
   );
