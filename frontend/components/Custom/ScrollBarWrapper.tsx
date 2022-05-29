289a7c79e2f6b33e366d1d9259c5632dc72f2d76
Atechchatterjee <atechchatterjeee@gmail.com>
2022-05-29 22:07:33 +0530

    style: change overflow to auto

--- frontend/components/Custom/ScrollBarWrapper.tsx
+++ frontend/components/Custom/ScrollBarWrapper.tsx
@@ -2,7 +2,7 @@ import { Box, BoxProps } from "@chakra-ui/react";
 import { scrollBarStyle } from "../../util/ScrollBarStyle";
 
 const ScrollBarWrapper = ({ children, ...props }: BoxProps) => (
-  <Box sx={scrollBarStyle()} position="sticky" overflowY="scroll" {...props}>
+  <Box sx={scrollBarStyle()} position="sticky" overflow="auto" {...props}>
     <Box height="100vh">{children}</Box>
   </Box>
 );
