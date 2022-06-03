a6b37c4bb2100d7bd1cdd8feb27ab2ba2b8b7ee8
Atechchatterjee <atechchatterjeee@gmail.com>
2022-06-03 21:19:13 +0530

    refactor: import getAllCategory from category service

--- frontend/hooks/useCategoryData.ts
+++ frontend/hooks/useCategoryData.ts
@@ -1,45 +1,35 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
 import { Category } from "../types/shop";
-import constants from "../util/Constants";
 import { CategoryTree, convertToCustomTree } from "../util/Tree";
+import { getAllCategory } from "../services/CategoryService";
 
 interface useCategoryDataProps {
   refetch?: boolean;
   returnTree?: boolean;
 }
 
-export const useCategoryData = ({refetch, returnTree}: useCategoryDataProps={}): [
+export const useCategoryData = ({
+  refetch,
+  returnTree,
+}: useCategoryDataProps = {}): [
   categoryData: Category[],
   setCategoryData: (_: Category[]) => void,
-  categoryTree?: CategoryTree,
+  categoryTree?: CategoryTree
 ] => {
   const [categoryData, setCategoryData] = useState<Category[]>([]);
   const [categoryTree, setCategoryTree] = useState<CategoryTree>();
 
-  const getAllCategory = async (): Promise<Category[]> => (
-    new Promise(async (resolve, reject) => {
-      try {
-        const res = await axios.get(`${constants.url}/shop/getallcategory/`, {
-          withCredentials: true,
-        });
-        resolve(res.data.categories);
-      } catch (err) {
-        reject(err);
-      }
-    })
-  );
-
   useEffect(() => {
-    getAllCategory().then(
-      (categoryData) => {
+    getAllCategory()
+      .then((categoryData) => {
         setCategoryData(categoryData);
-        if(returnTree) {
+        if (returnTree) {
           const customTree = convertToCustomTree(categoryData);
           setCategoryTree(customTree);
         }
-      }).catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
   }, [refetch]);
 
   return [categoryData, setCategoryData, returnTree ? categoryTree : undefined];
-}
+};
