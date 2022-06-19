df603c2a5e23b8715eed55804831a642e7b9769a
Atechchatterjee <atechchatterjeee@gmail.com>
2022-06-19 22:13:08 +0530

    feat: use use-query hook for data-fetching

--- frontend/components/Admin/AllProducts.tsx
+++ frontend/components/Admin/AllProducts.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import Product from "../Shop/Product";
 import {
   Button,
@@ -18,19 +19,16 @@ import { getAllProducts, deleteProduct } from "../../services/ProductService";
 import usePagination from "../../hooks/usePagination";
 import PaginationBar from "../Widgets/PaginationBar";
 
-const fetchAllProducts = async (): Promise<any[]> => {
-  const allProducts = await getAllProducts(13);
-  return Promise.resolve(allProducts);
-};
-
 const AllProducts: React.FunctionComponent = () => {
-  const [allProducts, setAllProducts] = useState<any[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [del, setDel] = useState<boolean>(false);
   const [prdDel, setPrdDel] = useState<any>();
   const [columns] = useDynamicColumns(4, [1700, 1300, 860]);
   const [pageLimit] = useState<number>(8);
-  useState<number>(pageLimit);
+  const { data: allProducts, isLoading } = useQuery(
+    ["all-products", del],
+    async () => await getAllProducts()
+  );
 
   // modal to confirm delete-action
   const DeleteModal: React.FC<{ confirmDelete: Function }> = ({
@@ -60,22 +58,8 @@ const AllProducts: React.FunctionComponent = () => {
     );
   };
 
-  // removes the product from the local array of product values
-  const removeProduct = (product: any | undefined) => {
-    if (!product) return;
-    let copyProducts: any[] = allProducts.filter((el) => {
-      return product.product_id !== el.product_id;
-    });
-    setAllProducts(copyProducts);
-  };
-
-  useEffect(() => {
-    fetchAllProducts().then((allProducts) => setAllProducts(allProducts));
-  }, []);
-
   useEffect(() => {
     if (del) {
-      removeProduct(prdDel);
       setDel(false);
     }
   }, [del, setDel, prdDel, setPrdDel]);
@@ -85,10 +69,11 @@ const AllProducts: React.FunctionComponent = () => {
     endIndx,
     moveNext,
     movePrev,
+    moveToPage,
     numberOfPages,
     currentSelectedPage,
     numberOfElementDisplayed,
-  } = usePagination({ pageLimit, maxEl: allProducts.length });
+  } = usePagination({ pageLimit, maxEl: allProducts?.length || 0 });
 
   return (
     <Box marginTop="3em" width="100%" overflow="hidden" padding="0 2% 3% 2%">
@@ -100,28 +85,40 @@ const AllProducts: React.FunctionComponent = () => {
           numberOfElementDisplayed / columns
         )}, 1fr)`}
       >
-        {allProducts
-          .filter((_, i: number) => i >= startIndx && i < endIndx)
-          .map((product) => (
-            <ProductInfoContext.Provider
-              value={{
-                productInfo: [{ ...product, id: product.product_id }, () => {}],
-              }}
-            >
-              <Product
-                key={product.product_id}
-                onDelete={() => {
-                  setIsOpen(true);
-                  setPrdDel(product);
+        {allProducts &&
+          allProducts
+            .filter((_: any, i: number) => i >= startIndx && i < endIndx)
+            .map((product: any) => (
+              <ProductInfoContext.Provider
+                value={{
+                  productInfo: [
+                    { ...product, id: product.product_id },
+                    () => {},
+                  ],
                 }}
-                editable
-              />
-            </ProductInfoContext.Provider>
-          ))}
+              >
+                <Product
+                  key={product.product_id}
+                  onDelete={() => {
+                    setIsOpen(true);
+                    setPrdDel(product);
+                  }}
+                  editable
+                />
+              </ProductInfoContext.Provider>
+            ))}
       </Grid>
-      <PaginationBar
-        {...{ moveNext, movePrev, currentSelectedPage, numberOfPages }}
-      />
+      {!isLoading && (
+        <PaginationBar
+          {...{
+            moveNext,
+            movePrev,
+            moveToPage,
+            currentSelectedPage,
+            numberOfPages,
+          }}
+        />
+      )}
       <DeleteModal
         confirmDelete={() => {
           setDel(true);
