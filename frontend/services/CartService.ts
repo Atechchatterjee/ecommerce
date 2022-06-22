773073dcd54eff66e1547fcb1535942d82de64a4
Atechchatterjee <atechchatterjeee@gmail.com>
2022-06-22 22:39:12 +0530

    feat: add functions to get and delete cart items

--- frontend/services/CartService.ts
+++ frontend/services/CartService.ts
@@ -1,3 +1,4 @@
+import { CartItem } from "../types/shop";
 import api from "../util/AxiosApi";
 
 export const addProductToCart = async (quantity: number, productId: number) => {
@@ -5,7 +6,18 @@ export const addProductToCart = async (quantity: number, productId: number) => {
     "/shop/add-to-cart/",
     {
       product_id: productId,
-      quantity,
+      quantity: quantity,
+    },
+    { withCredentials: true }
+  );
+  return Promise.resolve(res);
+};
+
+export const deleteItemsFromCart = async (productIds: number[]) => {
+  const res = await api.post(
+    "/shop/delete-cart-items/",
+    {
+      product_ids: productIds,
     },
     { withCredentials: true }
   );
@@ -21,4 +33,12 @@ export const checkIfInCart = async (productId: number) => {
     { withCredentials: true }
   );
   return Promise.resolve(res);
+};
+
+export const getCartItems = async (): Promise<CartItem[][] | null> => {
+  const res = await api.get("/shop/get-products-from-cart/", {
+    withCredentials: true,
+  });
+  if (res) return Promise.resolve(res.data.cart_items);
+  else return Promise.resolve(null);
 };
