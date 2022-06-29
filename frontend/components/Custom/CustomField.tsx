46792e0ef803d47071645f8340c803018f3b81bc
Atechchatterjee <atechchatterjeee@gmail.com>
2022-06-29 21:57:21 +0530

    feat: add props for loading

--- frontend/components/Custom/CustomField.tsx
+++ frontend/components/Custom/CustomField.tsx
@@ -1,14 +1,23 @@
-import { FormControl, FormLabel, Input, InputProps } from "@chakra-ui/react";
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  InputProps,
+  Spinner,
+} from "@chakra-ui/react";
 import React from "react";
 
 interface CustomFieldProps extends InputProps {
   label?: string;
+  isLoading?: boolean;
 }
 
 export const CustomField = ({
   isRequired,
   label,
   mt,
+  isLoading,
   ...props
 }: CustomFieldProps) => {
   return (
@@ -25,15 +34,31 @@ export const CustomField = ({
       ) : (
         <></>
       )}
-      <Input
-        variant="outline"
-        size="md"
-        borderRadius="sm"
-        focusBorderColor="secondary.200"
-        errorBorderColor="red.300"
-        fontSize={{ base: "13px", md: "15px", lg: "16px" }}
-        {...props}
-      />
+      {isLoading ? (
+        <Flex flexDirection="row" gridGap={3}>
+          <Input
+            flex="1"
+            variant="outline"
+            size="md"
+            borderRadius="sm"
+            focusBorderColor="secondary.200"
+            errorBorderColor="red.300"
+            fontSize={{ base: "13px", md: "15px", lg: "16px" }}
+            {...props}
+          />
+          <Spinner size="md" mt="2%" />
+        </Flex>
+      ) : (
+        <Input
+          variant="outline"
+          size="md"
+          borderRadius="sm"
+          focusBorderColor="secondary.200"
+          errorBorderColor="red.300"
+          fontSize={{ base: "13px", md: "15px", lg: "16px" }}
+          {...props}
+        />
+      )}
     </FormControl>
   );
 };
