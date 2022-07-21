f13ffb482e7baec8e3c3574b77aabc21ecf12a24
Atechchatterjee <atechchatterjeee@gmail.com>
2022-07-21 23:00:43 +0530

    feat: add max length for char fields

--- backend/src/checkout/models.py
+++ backend/src/checkout/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Shipping_Details(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('authentication.User', verbose_name="user_id", on_delete=models.SET_NULL)
+    user_id = models.ForeignKey('authentication.User', verbose_name="user_id", on_delete=models.CASCADE)
     address = models.TextField(null=False, blank=False)
     pincode = models.CharField(null=False, blank=False, max_length=6)
-    country = models.CharField(null=False, blank=False)
-    state = models.CharField(null=False, blank=False)
-    city = models.CharField(null=False, blank=False)
+    country = models.CharField(null=False, blank=False, max_length=100)
+    state = models.CharField(null=False, blank=False, max_length=100)
+    city = models.CharField(null=False, blank=False, max_length=100)
 
 class Shipping_Query(models.Model):
     id = models.AutoField(primary_key=True)
