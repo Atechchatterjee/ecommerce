475a78972db7aa4c0cf046b74d00e943a8d256dc
Atechchatterjee <atechchatterjeee@gmail.com>
2022-07-24 23:07:13 +0530

    feat: add support for left icon

--- frontend/components/Custom/CustomField.tsx
+++ frontend/components/Custom/CustomField.tsx
@@ -3,6 +3,8 @@ import {
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputLeftElement,
   InputProps,
   Spinner,
 } from "@chakra-ui/react";
@@ -11,6 +13,7 @@ import React from "react";
 interface CustomFieldProps extends InputProps {
   label?: string;
   isLoading?: boolean;
+  leftIcon?: string;
 }
 
 export const CustomField = ({
@@ -18,6 +21,7 @@ export const CustomField = ({
   label,
   mt,
   isLoading,
+  leftIcon,
   ...props
 }: CustomFieldProps) => {
   return (
@@ -30,8 +34,39 @@ export const CustomField = ({
       {label ? <FormLabel fontSize="0.87em">{label}</FormLabel> : <></>}
       {isLoading ? (
         <Flex flexDirection="row" gridGap={3}>
+          <InputGroup>
+            {leftIcon && (
+              <InputLeftElement
+                pointerEvents="none"
+                color="gray.400"
+                fontSize="1.2em"
+                children={leftIcon}
+              />
+            )}
+            <Input
+              flex="1"
+              variant="outline"
+              size="md"
+              borderRadius="sm"
+              focusBorderColor="secondary.200"
+              errorBorderColor="red.300"
+              fontSize={{ base: "13px", md: "15px", lg: "16px" }}
+              {...props}
+            />
+            <Spinner size="md" mt="2%" />
+          </InputGroup>
+        </Flex>
+      ) : (
+        <InputGroup>
+          {leftIcon && (
+            <InputLeftElement
+              pointerEvents="none"
+              color="gray.400"
+              fontSize="1.2em"
+              children={leftIcon}
+            />
+          )}
           <Input
-            flex="1"
             variant="outline"
             size="md"
             borderRadius="sm"
@@ -40,18 +75,7 @@ export const CustomField = ({
             fontSize={{ base: "13px", md: "15px", lg: "16px" }}
             {...props}
           />
-          <Spinner size="md" mt="2%" />
-        </Flex>
-      ) : (
-        <Input
-          variant="outline"
-          size="md"
-          borderRadius="sm"
-          focusBorderColor="secondary.200"
-          errorBorderColor="red.300"
-          fontSize={{ base: "13px", md: "15px", lg: "16px" }}
-          {...props}
-        />
+        </InputGroup>
       )}
     </FormControl>
   );
