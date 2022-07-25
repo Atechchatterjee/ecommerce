4c91ff9710d4d8357e74899cfbd657e1e2a64bcf
Atechchatterjee <atechchatterjeee@gmail.com>
2022-07-25 22:41:29 +0530

    feat: add shipping details serializer

--- backend/src/checkout/serializers.py
+++ backend/src/checkout/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from checkout.models import Shipping_Query
+from checkout.models import Shipping_Query, Shipping_Details
 
 class ShippingQuerySerializers(serializers.ModelSerializer):
     class Meta:
         model = Shipping_Query
         fields = "__all__"
         depth = 2
+
+class ShippingDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shipping_Details
+        fields = "__all__"
