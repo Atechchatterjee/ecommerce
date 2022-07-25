ea8a98da0ce7ffec381cd16db4ec5ce107690cac
Atechchatterjee <atechchatterjeee@gmail.com>
2022-07-25 22:42:39 +0530

    feat: add more api services

--- frontend/services/ShippingService.ts
+++ frontend/services/ShippingService.ts
@@ -15,10 +15,31 @@ export const createShippingQuery = async (shippingDetails: ShippingDetails) => {
   return Promise.resolve();
 };
 
-export const getAllShippingQueries = async (): Promise<any> => {
+export const getAllShippingQueries = async (): Promise<any[]> => {
   const res = await api.get(`/checkout/get-all-shipping-queries/`, {
     withCredentials: true,
   });
-  if (res) return Promise.resolve(res);
+  if (res.status === 200) return Promise.resolve(res.data.shipping_queries);
+  else return Promise.reject();
+};
+
+export const approveShippingQueries = async (
+  shippingQueries: { id: number; charges: string }[]
+) => {
+  const res = await api.post(
+    `/checkout/approve-shipping-queries/`,
+    { queries: shippingQueries },
+    { withCredentials: true }
+  );
+  if (res.status === 200) return Promise.resolve();
+  else return Promise.reject();
+};
+
+export const getShippingDetails = async (): Promise<any> => {
+  const res = await api.get(`/checkout/get-shipping-details/`, {
+    withCredentials: true,
+  });
+  if (res && res.status === 200)
+    return Promise.resolve(res.data.shipping_details);
   else return Promise.reject();
 };
